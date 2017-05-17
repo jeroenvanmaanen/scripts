95b9f0320b00dfbc243f4b2f42d1207409679a77
Jeroen van Maanen <jeroen@sollunae.net>
2017-05-17 10:53:01 +0200

    Adapted "sfind.sh" to run also on Windows.

--- sfind.sh
+++ sfind.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+set -x
+
+FIND_EXT_BEFORE=()
+FIND_EXT_AFTER=(--regex-type posix-egrep)
+case $(uname) in
+Darwin*)
+    FIND_EXT_BEFORE=(-E)
+    FIND_EXT_AFTER=()
+esac
+export SED_EXT
+
 declare -a PATHS
 N=0
 while [ "$#" -gt 0 -a "::${1#-}" = "::$1" -a "::$1" != "::(" -a "::$1" != '::!' ]
@@ -12,4 +23,4 @@ done
 ## do
 ##	echo ">> $D" >&2
 ## done
-/usr/bin/find -E "${PATHS[@]}" \( -type d \( -name .svn -o -name tmp \) -prune \! -type d \) -o \( \( \! -type d -o \! -name .svn \) "$@" \)
+/usr/bin/find "${FIND_EXT_BEFORE[@]}" "${PATHS[@]}" "${FIND_EXT_AFTER[@]}" \( -type d \( -name .svn -o -name tmp \) -prune \! -type d \) -o \( \( \! -type d -o \! -name .svn \) "$@" \)
