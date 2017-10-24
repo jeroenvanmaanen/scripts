de3041ffb54011986088639cd9e7028cffaeb23a
Jeroen van Maanen <jeroen@sollunae.net>
2017-10-24 11:21:30 +0200

    Small fix.

--- triodos-and-ing.sh
+++ triodos-and-ing.sh
@@ -4,6 +4,6 @@ BIN="$(cd "$(dirname "$0")" ; pwd)"
 
 head -1 Alle_rekeningen* | tr ',' '\011'
 (
-    cat Alle_rekeningen* | "${BIN}/csv-to-flat.sh" | "${BIN}/flat-to-tsv.sh"
+    cat Alle_rekeningen* | "${BIN}/csv-to-flat.sh" | "${BIN}/flat-to-tsv.sh" | tail +2
     cat mutations* | "${BIN}/triodos-to-ing.sh"
 ) | sort
