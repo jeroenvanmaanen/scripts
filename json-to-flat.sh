c28aaa79bcb268f4f92f7ae20d2250a05834bb37
Jeroen van Maanen <jeroen@sollunae.net>
2018-01-22 14:34:25 +0100

    Fixed handling of standard input for "json-to-flat.sh".

--- json-to-flat.sh
+++ json-to-flat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 DIR="$(pwd)"
 
-docker run --rm -v "${DIR}:${DIR}" -w "$DIR" jeroenvm/jsontoflat:0.1-SNAPSHOT "$@"
+docker run --rm -v "${DIR}:${DIR}" -w "$DIR" -i jeroenvm/jsontoflat:0.1-SNAPSHOT "$@"
