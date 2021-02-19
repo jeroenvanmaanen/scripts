9aff3cee5ea42e853ccc18ce3a6ba87c46dcde8d
Jeroen van Maanen <jeroen@leialearns.org>
2021-02-19 19:42:14 +0100

    Added support for "--verbose" flag to "metapost.sh".

--- metapost.sh
+++ metapost.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-set -x
-
 BIN="$(cd "$(dirname "$0")" ; pwd)"
+
+FLAGS_INHERIT=()
+source "${BIN}/verbose.sh"
 
 HOME_DIR="$(cd ~ ; pwd)"
 
-"${BIN}/run-docker-wrapped-command.sh" -v "${HOME_DIR}:/root" metapost /usr/bin/mpost "$@"
+DOCKER_FLAGS=()
+if "${SILENT}"
+then
+	:
+else
+	DOCKER_FLAGS[${#DOCKER_FLAGS[@]}]='--verbose'
+fi
+"${BIN}/run-docker-wrapped-command.sh" "${DOCKER_FLAGS[@]}" -v "${HOME_DIR}:/root" metapost /usr/bin/mpost "$@"
 
 for F in *.*[0-9]
 do
