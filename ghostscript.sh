d941ace0248fda87f28656631fae05523d9e3d14
Jeroen van Maanen <jeroen@leialearns.org>
2021-08-17 07:45:26 +0200

    Mount ~/src instead of ~.

--- ghostscript.sh
+++ ghostscript.sh
@@ -5,7 +5,7 @@ BIN="$(cd "$(dirname "$0")" ; pwd)"
 FLAGS_INHERIT=()
 source "${BIN}/verbose.sh"
 
-HOME_DIR="$(cd ~ ; pwd)"
+SRC_DIR="$(cd ~/src ; pwd)"
 
 DOCKER_FLAGS=()
 if "${SILENT}"
@@ -14,4 +14,4 @@ then
 else
 	DOCKER_FLAGS[${#DOCKER_FLAGS[@]}]='--verbose'
 fi
-"${BIN}/run-docker-wrapped-command.sh" "${DOCKER_FLAGS[@]}" -v "${HOME_DIR}:/root" metapost /usr/bin/gs "$@"
+"${BIN}/run-docker-wrapped-command.sh" "${DOCKER_FLAGS[@]}" -v "${SRC_DIR}:${SRC_DIR}" metapost /usr/bin/gs "$@"
--- metapost.sh
+++ metapost.sh
@@ -5,7 +5,7 @@ BIN="$(cd "$(dirname "$0")" ; pwd)"
 FLAGS_INHERIT=()
 source "${BIN}/verbose.sh"
 
-HOME_DIR="$(cd ~ ; pwd)"
+SRC_DIR="$(cd ~/src ; pwd)"
 
 DOCKER_FLAGS=()
 if "${SILENT}"
@@ -14,7 +14,7 @@ then
 else
 	DOCKER_FLAGS[${#DOCKER_FLAGS[@]}]='--verbose'
 fi
-"${BIN}/run-docker-wrapped-command.sh" "${DOCKER_FLAGS[@]}" -v "${HOME_DIR}:/root" metapost /usr/bin/mpost "$@"
+"${BIN}/run-docker-wrapped-command.sh" "${DOCKER_FLAGS[@]}" -v "${SRC_DIR}:${SRC_DIR}" metapost /usr/bin/mpost "$@"
 
 for F in *.*[0-9]
 do
