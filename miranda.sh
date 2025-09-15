00ee18a4fe27ce3d68328d01202fc999f051fcfd
Jeroen van Maanen <jeroen@leialearns.org>
2025-09-15 08:45:23 +0200

    Improved script `miranda.sh`

--- miranda.sh
+++ miranda.sh
@@ -6,4 +6,4 @@ BIN="$(cd "$(dirname "$0")" ; pwd)"
 
 source "${BIN}/lib-verbose.sh"
 
-ssh -t jeroen@192.168.178.76 workspace/doodle/home-node/truenas/bin/screen.sh "${FLAGS_INHERIT[@]}" "$@"
+ssh -t jeroen@192.168.178.76 workspace/doodle/home-node/truenas/bin/dev.sh "${FLAGS_INHERIT[@]}" "$@"
